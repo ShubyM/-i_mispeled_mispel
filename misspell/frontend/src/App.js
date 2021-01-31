02535aaef426337e6e0429a7fa0d4dda8b93a59f
Shubhranshu Mishra <shubymishra20@gmail.com>
2021-01-31 13:52:46 -0500

    added jimmy changes

--- misspell/frontend/src/App.js
+++ misspell/frontend/src/App.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import { BrowserRouter as Router, Link } from "react-router-dom";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="App">
-            <div id="title">
-                    i mispeled mispel
-            </div>
-            <div id="description">HackUCI project by Jimmy, Sunbun, Shubrangle<br></br>
-                Welcome to a fun quarantine spelling bee with your friends!<br></br>How to start: Click the play button to hear the word.<br></br>Rules: Do not search up the word spelling!
-                <br></br>Have fun you inconceiveable noosance
-            </div>
-            <input class="interact" id="playername" type="text" placeholder="enter player name" name="name"></input>
-            <Link to="/room" class="interact" id="waitroom">Create Room</Link> 
-            <Link to="/join" class="interact" id="joinroom">Join Room</Link>
-        </div>
-    );
-}
 
-export default App;
+export default class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: this.defaultName,
+        };
+        this.handleCreateRoom = this.handleCreateRoom.bind(this);
+        this.handleUserName = this.handleUserName.bind(this);
+    }
+
+    handleUserName(e) {
+        this.setState({
+            userName: e.target.value,
+        });
+    }
+
+    handleCreateRoom() {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify({
+                host_name: this.state.userName,
+            }),
+        };
+        fetch("/api/create-room", requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div id="title">
+                        i mispeled mispel
+                </div>
+                <div id="description">HackUCI project by Jimmy, Sunbun, Shubrangle<br></br>
+                    Welcome to a fun quarantine spelling bee with your friends!<br></br><strong>How to start:</strong> Click the play button to hear the word.<br></br><strong>Rules:</strong> Do not search up the word spelling!
+                    <br></br>Have fun you inconceiveable noosance
+                </div>
+                <input class="interact" id="playername" required={true} type="text" placeholder="Enter Player Name" onChange={this.handleUserName} defaultValue={this.defaultName}/>
+                <button class="interact" id="waitroom" onClick={this.handleCreateRoom}><Link to="/room">Create Room</Link></button>
+                <button class="interact" id="joinroom"><Link to="/join">Join Room</Link></button>
+            </div>
+        );
+    }
+}
--- misspell/frontend/src/pages/GameRoom.js
+++ misspell/frontend/src/pages/GameRoom.js
@@ -1,33 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./GameRoom.css";
 
-function GameRoom() {
-    return (
-        <div className="GameRoom">
-            <div id="spell">Spell the Word:</div>
-            <button id="playback"><span class="iconify" data-icon="openmoji:play-button" data-inline="false" height="105px" width="105px"></span></button>
-            <div class="example" id="exsent">Example:</div>
-            <div class="example" id="random">random sentence example fit</div>
-            <div id="enterword">Enter your word below</div>
-            <div id="empty"></div>
-            <div class="player" id="p1">player 1</div>
-            <div class="player" id="p2">player 2</div>
-            <div class="player" id="p3">player 3</div>
-            <div class="player" id="p4">player 4</div>
-            <div class="player" id="p5">player 5</div>
-            <div class="player" id="p6">player 6</div>
-            <div class="player" id="p7">player 7</div>
-            <div class="player" id="p8">player 8</div>
-            <div class="score" id="s1">scr1</div>
-            <div class="score" id="s2">scr2</div>
-            <div class="score" id="s3">scr3</div>
-            <div class="score" id="s4">scr4</div>
-            <div class="score" id="s5">scr5</div>
-            <div class="score" id="s6">scr6</div>
-            <div class="score" id="s7">scr7</div>
-            <div class="score" id="s8">scr8</div>
-        </div>
-    );
-}
 
-export default GameRoom;
+export default class GameRoom extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            randomWord: this.defaultWord,
+            randdefinition: this.defaultDefinition,
+        };
+        this.getRandomWord = this.getRandomWord.bind(this);
+    }
+    getRandomWord(e) {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                word: this.state.randomWord,
+                definition: this.state.randdefinition,
+            }),
+        };
+        fetch("/api/get-word", requestOptions)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }
+
+    render() {
+        return (
+            <div className="GameRoom">
+                <div id="spell">Spell the Word:</div>
+                <button id="playback"><span class="iconify" data-icon="openmoji:play-button" data-inline="false" height="105px" width="105px"></span></button>
+                <div class="example" id="exsent">Example:</div>
+                <div class="example" id="random">random sentence example fit</div>
+                <div id="enterword">Enter your word below</div>
+                <div id="empty"></div>
+                <div class="player" id="p1">player 1</div>
+                <div class="player" id="p2">player 2</div>
+                <div class="player" id="p3">player 3</div>
+                <div class="player" id="p4">player 4</div>
+                <div class="player" id="p5">player 5</div>
+                <div class="player" id="p6">player 6</div>
+                <div class="player" id="p7">player 7</div>
+                <div class="player" id="p8">player 8</div>
+                <div class="score" id="s1">scr1</div>
+                <div class="score" id="s2">scr2</div>
+                <div class="score" id="s3">scr3</div>
+                <div class="score" id="s4">scr4</div>
+                <div class="score" id="s5">scr5</div>
+                <div class="score" id="s6">scr6</div>
+                <div class="score" id="s7">scr7</div>
+                <div class="score" id="s8">scr8</div>
+            </div>
+        );
+    }
+}
